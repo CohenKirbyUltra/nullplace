1af5f106a444ccce6a444a84d62ea8f76fdca6a5
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-05-22 17:21:40 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// Music & DOM Loaded Variable
+
+var DOMLoaded = false;
+
 var Music = new Audio();
 
 Music.src = "src/audio/lostandalone.mp3";
 Music.volume = 0.23;
 Music.autoplay = true;
 
-document.addEventListener("loaded", Music.play());
+document.addEventListener("click", function() {
+    if (!DOMLoaded) {
+        DOMLoaded = true;
+        Music.play();
+    }
+});
