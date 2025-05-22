4d3f9863f9e135c5ab444df67fae02f0e840890c
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-05-22 19:24:52 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+// Function
+
+var darkmode;
+
+function updateScheme() {
+    darkmode = localStorage.getItem("darkmode");
+
+    let all = document.querySelectorAll("*");
+
+    all.forEach((element) => {
+        if (darkmode = false) {
+            element.setAttribute("style", "color-scheme: light;");
+        } else {
+            element.setAttribute("style", "color-scheme: dark;");
+        }
+    });
+}
+
 // Music & DOM Loaded Variable
 
 var DOMLoaded = false;
 
 var Music = new Audio();
+
+if (localStorage.getItem("darkmode").length > 0) {
+    updateScheme();
+} else {
+    localStorage.setItem("darkmode", false);
+    updateScheme();
+}
 
 Music.src = "src/audio/lostandalone.mp3";
 Music.volume = 0.23;
@@ -32,4 +57,6 @@ document.body.addEventListener("click", async () => {
             );
             DOMPlease.remove();
         }
-    });
+});
+
+document.getElementById("darkmode").addEventListener("click", updateScheme);
