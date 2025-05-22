be8f1a496e8ff091e7a3d90415c52b3293461227
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-05-22 19:55:10 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
-const root = document.documentElement;
-const rootStyle = window.getComputedStyle(root);
-
-// Get a specific CSS property value
-const colorScheme = rootStyle.getPropertyValue('color-scheme');
-console.log(colorScheme);
-
-const darkimg = document.getElementById("img-darkmode");
-
-// Function
-
-var darkmode;
-
-function updateScheme(bool) {
-    if (bool) {
-        darkmode = localStorage.getItem("darkmode");
-    } else {
-        darkmode = false;
-    }
-
-    if (darkmode) {
-        darkimg.src = "src/images/svg/Moon.svg";
-        root.style.setProperty('color-scheme', 'dark');
-    } else {
-        darkimg.src = "src/images/svg/Sun.svg";
-        root.style.setProperty('color-scheme', 'light');
-    }
-}
-
-
-
 // Music & DOM Loaded Variable
 
 var DOMLoaded = false;
 
 var Music = new Audio();
-
-if (localStorage.getItem("darkmode") !== undefined || localStorage.getItem("darkmode") !== null) {
-    updateScheme(false);
-} else {
-    localStorage.setItem("darkmode", false);
-    updateScheme(true);
-}
 
 Music.src = "src/audio/lostandalone.mp3";
 Music.volume = 0.23;
@@ -70,16 +32,4 @@ document.body.addEventListener("click", async () => {
             );
             DOMPlease.remove();
         }
-});
-
-var change = false;
-
-document.getElementById("darkmode").addEventListener("click", function () {
-    if (!change) { 
-        darkmode = true;
-        localStorage.setItem("darkmode", true);
-    } else {
-        darkmode = false;
-        localStorage.setItem("darkmode", false);
-    }
 });
