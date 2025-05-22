ac1a1c16bab275ff78eca2e92915b3aa498084f2
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-05-22 17:33:16 -0300

    a

--- src/index.js
+++ src/index.js
@@ -7,10 +7,23 @@ var Music = new Audio();
 Music.src = "src/audio/lostandalone.mp3";
 Music.volume = 0.23;
 Music.autoplay = true;
+Music.loop = true;
 
-document.body.addEventListener("click", function() {
-    if (!DOMLoaded) {
-        DOMLoaded = true;
-        Music.play();
-    }
-});
+// Dom Load Element
+
+const DOMPlease = document.getElementById("LoadPlease");
+
+document.body.addEventListener("click", async () => {
+        if (!DOMLoaded) {
+            DOMLoaded = true;
+            Music.play();
+            await DOMPlease.animate([{ opacity: 1 }, { opacity: 2 }],
+                {
+                    fill: "forwards",
+                    easing: "steps(4, end)",
+                    duration: 1, s,
+                }
+            );
+            DOMPlease.remove();
+        }
+    });
