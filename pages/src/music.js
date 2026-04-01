2bc54268792c264ca4b2708be981cb2936430b02
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2026-04-01 13:17:15 -0300

    a

--- pages/src/music.js
+++ pages/src/music.js
@@ -1,21 +1,11 @@
-export default class Music {
-    constructor(src, volume, loop) {
-        this.src = src;
-        this.volume = volume;
-        this.loop = loop;
+export default function Music {
+    this.src = src;
+    this.volume = volume;
+    this.loop = loop;
 
-        const music = new Audio(this.src);
-        music.volume = this.volume
-        music.loop = this.loop;
+    const music = new Audio(this.src);
+    music.volume = this.volume
+    music.loop = this.loop;
 
-        document.addEventListener("click", this.play())
-    }
-
-    play() {
-        music.play()
-    }
-
-    stop() {
-        music.stop()
-    }
+    document.addEventListener("click", music.play())
 }
